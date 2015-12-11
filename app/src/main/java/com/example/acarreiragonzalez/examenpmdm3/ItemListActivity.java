ac39d8fa3d4609d9b440fdd033e71072685ce536
acarreiragonzalez <acarreiragonzalez@danielcastelao.org>
2015-12-11 13:57:15 +0100

    Añadese o toast que indica que a pantalla esta en land co mensaxe "Tumbado" co metodo girar() no ItemListActivity

--- app/src/main/java/com/example/acarreiragonzalez/examenpmdm3/ItemListActivity.java
+++ app/src/main/java/com/example/acarreiragonzalez/examenpmdm3/ItemListActivity.java
@@ -55,32 +55,29 @@ public class ItemListActivity extends AppCompatActivity
         });
 
         if (findViewById(R.id.item_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
+
             mTwoPane = true;
 
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
+
             ((ItemListFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.item_list))
                     .setActivateOnItemClick(true);
         }
+        girar();
 
         // TODO: If exposing deep links into your app, handle intents here.
     }
 
-    /**
-     * Callback method from {@link ItemListFragment.Callbacks}
-     * indicating that the item with the given ID was selected.
-     */
+//sempre e cando a variable variablePantalla estea en modo true, amosara o toast "Tumbado"
+    public void girar() {
+        if (getResources().getBoolean(R.bool.variablePantalla)) {
+            Toast.makeText(getApplicationContext(), "Tumbado", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     public void onItemSelected(String id) {
         if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
+
             Bundle arguments = new Bundle();
             arguments.putString(ItemDetailFragment.ARG_ITEM_ID, id);
             ItemDetailFragment fragment = new ItemDetailFragment();
@@ -96,11 +93,20 @@ public class ItemListActivity extends AppCompatActivity
             detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
         }
+
+
     }
+
+
+
+
 
     @Override
     public void oK(String link) {
         //Co metodo makeText da clase Toast mandamos un mensaje instantaneo o executarse o metodo oK que nos mandará dita mensaxe
         Toast.makeText(getApplicationContext(), link, Toast.LENGTH_LONG).show();
     }
+
+
+
 }
